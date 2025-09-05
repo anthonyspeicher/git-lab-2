3f2b03ae23663932379719bf20e723b97435b770
anthonyspeicher <as905024@ohio.edu>
2025-09-05 14:45:06 -0400

    git-lab-program written

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Anthony Speicher
+ *        Date: 09/05/2025
+ * Description: Lab 2 Git Testing
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
